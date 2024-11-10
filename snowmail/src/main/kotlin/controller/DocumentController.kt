8e1554a0397ea7ccfbc6fc575e030d2187df9ada
sourishdas07 <109391867+sourishdas07@users.noreply.github.com>
2024-11-10 15:10:30 -0500

    disable tests for now

--- snowmail/src/main/kotlin/controller/DocumentController.kt
+++ snowmail/src/main/kotlin/controller/DocumentController.kt
@@ -4,6 +4,7 @@ import ca.uwaterloo.persistence.IDocumentRepository
 import integration.SupabaseClient
 import kotlinx.coroutines.runBlocking
 import java.io.File
+import java.nio.file.Paths
 
 class DocumentController(private val documentRepository: IDocumentRepository) {
 
@@ -36,23 +37,47 @@ fun main() = runBlocking<Unit> {
     val dbStorage = SupabaseClient()
     val documentController = DocumentController(dbStorage.documentRepository)
 
-    val bucket = "user_documents"
-    val path = "test/resume/test-resume.pdf"
-    val file = File(System.getProperty("user.home") + "/Desktop/test-resume.pdf")
+//    val bucket = "user_documents"
+//    val path = "Q6-2.jpg"
+//    val file = File(System.getProperty("user.home") + "/Desktop/Q6-2.jpg")
+//
+//    // Call uploadDocument and print the result
+//    val result = documentController.uploadDocument(bucket, path, file)
+//    result.onSuccess {
+//        println("Upload successful: $it")
+//    }.onFailure { error ->
+//        println("Error uploading document: ${error.message}")
+//    }
 
-    // Call uploadDocument and print the result
-    val result = documentController.uploadDocument(bucket, path, file)
-    result.onSuccess {
-        println("Upload successful: $it")
-    }.onFailure { error ->
-        println("Error uploading document: ${error.message}")
-    }
 
-    val deleteResult = documentController.deleteDocument(bucket, path)
-    deleteResult.onSuccess {
-        println("Delete successful: $it")
-    }.onFailure { error ->
-        println("Error deleting document: ${error.message}")
-    }
+//    val bucket = "user_documents"
+//    val path = "test-resume.pdf"
+//    // val resource = DocumentController::class.java.classLoader.getResource("test-resume.pdf")
+//    val resource = this::class.java.classLoader.getResource("test-resume.pdf")
+//
+//
+//    println(resource)
+//
+//    if (resource != null) {
+//        val file = Paths.get(resource.toURI()).toFile()
+//
+//        // Call uploadDocument and print the result
+//        val uploadResult = documentController.uploadDocument(bucket, path, file)
+//        uploadResult.onSuccess {
+//            println("Upload successful: $it")
+//        }.onFailure { error ->
+//            println("Error uploading document: ${error.message}")
+//        }
+//    } else {
+//        println("Resource file not found: test-resume.pdf")
+//    }
 
 }
+
+//    val deleteResult = documentController.deleteDocument(bucket, path)
+//    deleteResult.onSuccess {
+//        println("Delete successful: $it")
+//    }.onFailure { error ->
+//        println("Error deleting document: ${error.message}")
+//    }
+
